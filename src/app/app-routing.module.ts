f3e3bc7a597eb597ade8a29aa4cfe00806a02bc9
Jocelyn Keung <jocelyn.keung@captechventures.com>
2018-02-08 22:32:54 -0500

    Adding material theme and bootstrap style sheet; adding landing page and shopping component; adding routing module with routes to two modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {ShoppingComponent} from "./shopping/shopping.component";
+
+const routes: Routes = [
+  {path: 'about', component: LandingPageComponent},
+  {path: 'browse', component: ShoppingComponent }
+  {path: '', pathMatch: 'full', redirectTo: 'about'}
+];
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false})]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MatTabLink, MatTabNav, MatTabsModule, MatToolbarModule} from "@angular/material";
+import {AppRoutingModule} from "./app-routing.module";
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ShoppingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatTabsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
